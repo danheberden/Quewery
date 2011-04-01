2fa34c923b796ad85cbe15ee10f70c1e09396cdf
Dan Heberden <danheberden@gmail.com>
2011-04-01 11:16:05 -0700

    Fixed regex to disallow [ for getting by id

--- quewery.js
+++ quewery.js
@@ -5,7 +5,7 @@
 */
 (function( document, w ){
 	w.Quewery = function( selector ) {
-  		if( selector.match(/#[^(.|\s)]+$/) ) {
+  		if( selector.match(/#[^(.|\s|\[])]+$/) ) {
 			return [ document.getElementById( selector.substring(1) ) ];
 		} else {
 			return document.querySelectorAll( selector );
