8396ad3548bce81d025a8b84c88a184765beb645
Dan Heberden <danheberden@gmail.com>
2011-04-01 10:59:42 -0700

    It's Quewery

---
+++ quewery.js
@@ -0,0 +1,13 @@
+/*
+* Quewery.js (c) 2011 Dan Heberden
+*
+* Whatever license you want
+*/
+(function( document, w ){
+	w.Quewery = function( selector ) {
+    if( selector.match(/#[^\s]+$/) ) {
+        return [ document.getElementById( selector.substring(1) ) ];
+    } else {
+        return document.querySelectorAll( selector );
+    }   
+})( document );
