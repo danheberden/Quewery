88a3a41a6a7401bcb57807d91da168e1d64d9de3
Dan Heberden <danheberden@gmail.com>
2011-04-01 11:27:12 -0700

    removed tabs for ben_alman

--- quewery.js
+++ quewery.js
@@ -1,11 +1,11 @@
 /*
 * Quewery.js (c) 2011 Dan Heberden
 *
-* Whatever license you want, just make it a good one
+* Whatever license you want
 */
 (function( document, w ){
-	w.Quewery = function( selector ) {
-		return selector.match(/#[^(.|\s|\[)]+$/) ? [ document.getElementById( selector.substring(1) ) ] :
-			document.querySelectorAll( selector );  
-	}
+  w.Quewery = function( selector ) {
+    return selector.match(/#[^(.|\s|\[)]+$/) ? [ document.getElementById( selector.substring(1) ) ] :
+    document.querySelectorAll( selector );
+  }
 })( document, window);
