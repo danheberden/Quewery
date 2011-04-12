e66ce490a3e7c8552f130e792adadb6b36b6c034
Dan Heberden <danheberden@gmail.com>
2011-04-11 20:14:21 -0700

    Add window.Q alias and noConflict with allTheWay support

--- quewery.js
+++ quewery.js
@@ -3,14 +3,20 @@
 *
 * Whatever license you want
 */
-(function( document, w ) {
-  var oldQuewery = w.Quewery, Quewery = w.Quewery = function ( selector ) {
-    return /^#[\w\-]+$/.test(selector)
-      ? [ document.getElementById(selector.slice(1)) ]
-      : document.querySelectorAll(selector);
-  };
-  Quewery.noConflict = function() {
-    w.Quewery = oldQuewery;
+(function( document, window ) {
+	
+  var _Quewery = window.Quewery, 
+			_Q = window.Q,
+			Quewery = window.Q = window.Quewery = function ( selector ) {
+    		return /^#[\w\-]+$/.test( selector )
+      		? [ document.getElementById( selector.slice(1) ) ]
+      		: document.querySelectorAll( selector );
+  		};
+
+  Quewery.noConflict = function( allTheWay ) {
+    window.Q = _Q;
+		allTheWay && ( window.Quewery = _Quewery );
     return Quewery;
   };
+
 })( document, window );
