22655764e57765bb50953592ba3c984fb7a264a8
Martijn van der Ven <martijn@zegnat.net>
2014-02-12 18:54:51 +0100

    Case-insensitive & whitespace'd
    Generate all possible upper- and lowercase mixes.
    Update .noConflict to match.
    Reset whitespace.

--- quewery.js
+++ quewery.js
@@ -1,35 +1,57 @@
-
 /*
 * Quewery.js (c) 2011 Dan Heberden
 *
 * Whatever license you want
 */
-(function( document, window ) {
-	
-function makeArray(arr){
-    var ret=[],x;
 
-    for(x=0;x<arr.length;x++){
-        ret[x]=arr[x];
+(function (document, window) {
+
+  function makeArray(arr) {
+    var ret = [], x;
+    for (x = 0; x < arr.length; x++) {
+      ret[x] = arr[x];
+    }
+    return ret;
+  }
+
+  var lower = 'quewery',
+    upper = 'QUEWERY',
+    cases = 128,
+    names = [],
+    originals = {},
+    map,
+    i,
+    name,
+    Quewery = function (selector) {
+      return (/^#[\w\-]+$/).test(selector)
+        ? [ document.getElementById(selector.slice(1)) ]
+        : selector === 'body' && document.body
+          ? [ document.body ]
+          : !/\W/.test(selector)
+            ? makeArray(document.getElementsByTagName(selector))
+            : document.querySelectorAll(selector);
+    };
+
+    Quewery.noConflict = function (allTheWay) {
+      ['Q', 'q'].concat(allTheWay ? names : []).forEach(function (a) {
+        window[a] = originals[a];
+      });
+      return Quewery;
+    };
+
+    while (cases--) {
+        map = ('0000000' + cases.toString(2)).slice(-7);
+        i = 7;
+        name = '';
+        while (i--) { name = (map[i] === '1' ? upper[i] : lower[i]) + name; }
+        names.push(name);
+        originals[name] = window[name];
+        window[name] = Quewery;
     }
 
-    return ret;
-}
-
-  var _Quewery = window.Quewery, 
-			_Q = window.Q,
-			Quewery = window.Q = window.Quewery = function ( selector ) {
-    		return /^#[\w\-]+$/.test( selector )
-      		? [ document.getElementById( selector.slice(1) ) ]
-		: selector=="body"&&document.body?[document.body]
-      		: !/\W/.test(selector)? makeArray(document.getElementsByTagName(selector))
-                : document.querySelectorAll( selector );
-  		};
-
-  Quewery.noConflict = function( allTheWay ) {
-    window.Q = _Q;
-		allTheWay && ( window.Quewery = _Quewery );
-    return Quewery;
-  };
-
-})( document, window );
+    originals.Q = window.Q;
+    window.Q = Quewery;
+    originals.q = window.q;
+    window.q = Quewery;
+  
+}(document, window));
